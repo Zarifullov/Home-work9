bfeee70bc6e38a195a3273b4c3e165ef7544354b
Renat <rzarifullov@gmail.com>
2022-08-07 23:21:12 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,38 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int m = 0;
+int n = 0;
+int sum = 0;
+
+int.TryParse(Console.ReadLine(), out m);
+int.TryParse(Console.ReadLine(), out n);
+
+
+if (m > n) 
+{
+Console.WriteLine("Не корректный ввод");
+}
+else
+{
+
+PrintSum(m, n, sum);
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {sum}");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = int.Parse(Console.ReadLine());
+  return output;
+}
+}
