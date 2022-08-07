23a4fb719029e6345b53c1b401085c025f383751
Renat <rzarifullov@gmail.com>
2022-08-07 23:42:22 +0300

    Task3 added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,3 +2,26 @@
 
 // m = 2, n = 3 -> A(m,n) = 29
 
+int m = 0;
+int n = 0;
+
+int.TryParse(Console.ReadLine(), out m);
+int.TryParse(Console.ReadLine(), out n);
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = int.Parse(Console.ReadLine());
+  return output;
+}
