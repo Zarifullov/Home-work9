c60c61786d95689f432c47576dee0cea352ffbbf
Renat <rzarifullov@gmail.com>
2022-08-07 23:47:59 +0300

    Task3 added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -14,9 +14,13 @@ Console.Write($"Функция Аккермана = {functionAkkerman} ");
 
 int Ack(int m, int n)
 {
-  if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
+  if (m == 0) 
+    return n + 1;
+  else 
+    if (n == 0) 
+        return Ack(m - 1, 1);
+    else 
+        return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
